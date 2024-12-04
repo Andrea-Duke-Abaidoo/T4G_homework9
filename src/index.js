3db2f13af45da9612b6bb401517142f48c9641b1
Andrea-Duke-Abaidoo <andreadukeabaidoo@gmail.com>
2024-12-04 14:06:57 +0000

    DOM

---
+++ src/index.js
@@ -0,0 +1,29 @@
+let header = document.querySelector(".header")
+header.style.backgroundColor ="blue"
+header.style.color = "white"
+
+
+let fir = document.querySelector(".fir")
+fir.style.backgroundColor="grey"
+fir.innerHTML = "I know now what the DOM is🙌"
+fir.style.color = "black"
+
+let sec = document.getElementById("sec")
+sec.style.backgroundColor = "yellow"
+sec.innerHTML ="I know how to access/ select elements in the DOM👊"
+sec.style.color = "black"
+
+let thir = document.querySelector(".thir")
+thir.style.backgroundColor = "green"
+thir.innerHTML = "I know how to use the style methods to change the style of an element in the DOM✅"
+thir.style.color = "black"
+
+let forth = document.querySelector(".forth")
+forth.style.backgroundColor = "purple"
+forth.innerHTML = "I know how the styling properties differ in JS from CSS👍"
+forth.style.color = "white"
+
+let fif = document.querySelector(".fif")
+fif.style.backgroundColor = "black"
+fif.innerHTML = "Yes I did it!! I manipulated the DOM and styled my elements😍😍😍😍"
+fif.style.color = "white"
